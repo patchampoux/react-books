945c7a4f9383d2bcfda7303a967308c0e07cd86d
patchampoux <champoux.patrick@gmail.com>
2023-07-07 09:00:35 -0400

    Receiving New Titles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import {useState} from "react";
+import BookCreate from "./BookCreate";
+
 const App = () => {
+	const [books, setBooks] = useState([]);
+
+	const createBook = (title) => {
+		console.log('Need to add book with title of', title);
+	}
+
 	return (
-		<div>Hello World!</div>
+		<div>
+			<BookCreate onCreate={createBook} />
+		</div>
 	);
 }
 
--- src/BookCreate.js
+++ src/BookCreate.js
@@ -1,6 +1,28 @@
-const BookCreate = () => {
+import {useState} from "react";
+
+const BookCreate = ({ onCreate }) => {
+	const [title, setTitle] = useState("");
+
+	const handleChange = (e) => {
+		setTitle(e.target.value);
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		onCreate(title);
+
+		setTitle("");
+	}
+
 	return (
-		<div>BookCreate</div>
+		<div>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="book-title">Title</label>
+				<input type="text" id="book-titel" value={title} onChange={handleChange}/>
+				<button type="submit">Create!</button>
+			</form>
+		</div>
 	);
 }
 
